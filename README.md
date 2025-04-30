ef5b86b493d0cb06cef8fbbb11c5cab88892ccc7
Joseph Heinz <josephheinz28@gmail.com>
2025-04-30 10:14:28 -0400

    packaging semantics updates

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# neobrutalism-svelte
- 
+# Neobrutalism Components Svelte
+
+## Not at all affiliated with [ekmas](https://github.com/ekmas/neobrutalism-components)
+
+## Introduction
+This is a port of [ekmas/neobrutalism-components](https://github.com/ekmas/neobrutalism-components) to Svelte
+
+## Documentation
+Coming soon
+
+## License
+[MIT](LICENSE)
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,9 @@
 // place files you want to import through the `$lib` alias in this folder.
+//@ts-nocheck
+import Alert from "./components/alert/Alert.svelte";
+import AlertDescription from "./components/alert/AlertDescription.svelte";
+import AlertTitle from "./components/alert/AlertTitle.svelte";
+import Badge from "./components/Badge.svelte";
+import Button from "./components/Button.svelte";
+
+export {Alert, AlertDescription, AlertTitle, Badge, Button};
